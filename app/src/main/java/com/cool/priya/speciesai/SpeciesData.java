2b59671ad83267e95d1b0ddf67eae2560671f21b
Aditya Balodi <adityabalodi31@gmail.com>
2023-01-21 11:50:58 +0530

    Bird-Recognition

---
+++ app/src/main/java/com/cool/priya/speciesai/SpeciesData.java
@@ -0,0 +1,19 @@
+package com.cool.priya.speciesai;
+
+public class SpeciesData {
+    private String key;
+    private String value;
+
+    public SpeciesData(String key,String value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
